e69ee7337562abf8012897493a96c7217303601c
jonatan <jona.cc.93@gmail.com>
2021-09-06 23:54:52 -0300

    cambio

--- routes/crudRoutes.js
+++ routes/crudRoutes.js
@@ -1,14 +1,20 @@
-const {Router} = require('express')
+const { Router } = require("express");
 const router = Router();
 
-const {getNotes,createNotes,getNote,updateNote,deleteNote} = require('../controllers/crudControllers')
+const {
+  getNotes,
+  createNotes,
+  getNote,
+  updateNote,
+  deleteNote,
+} = require("../controllers/crudControllers");
 
-router.get('/', getNotes)
-router.post('/', createNotes);
+router.get("/", getNotes);
+router.post("/", createNotes);
 
-router.get('/:id', getNote);
-router.put('/:id', updateNote);
-router.delete('/:id', deleteNote);
+router.get("/:id", getNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+//hola
 
-
-module.exports =  router
+module.exports = router;
